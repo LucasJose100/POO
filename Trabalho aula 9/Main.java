9bc10218c4e8ad887075351503c7828b24669ddd
Lucas José <43818641+LucasJose100@users.noreply.github.com>
2019-11-04 19:48:30 -0300

    Main.java

---
+++ Trabalho aula 9/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Retangulo r1=new Retangulo(10,15);
+		r1.teste();
+}
+}
